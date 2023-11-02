60416393572eb0736904b5bda6d1460dd89812f5
Nguyen Tran <tkn2003@gmail.com>
2023-11-02 23:58:33 +0700

    Update UI

---
+++ app/src/main/java/com/example/attendanceapplication/Fragments/ProfileFragment.java
@@ -0,0 +1,44 @@
+package com.example.attendanceapplication.Fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.fragment.app.Fragment;
+
+import com.example.attendanceapplication.MainActivity;
+import com.example.attendanceapplication.R;
+import com.example.attendanceapplication.SecondActivity;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ProfileFragment extends Fragment {
+
+    private FirebaseAuth mAuth;
+
+    Button btnLogout;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        btnLogout = view.findViewById(R.id.btnLogout);
+        mAuth = FirebaseAuth.getInstance();
+
+        // Đăng xuất
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                startActivity(new Intent(getActivity(), MainActivity.class));
+                getActivity().finish();
+                Toast.makeText(getActivity(), "Logout Successful.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/attendanceapplication/SecondActivity.java
+++ app/src/main/java/com/example/attendanceapplication/SecondActivity.java
@@ -1,94 +1,51 @@
 package com.example.attendanceapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.example.attendanceapplication.Fragments.AttendanceFragment;
+import com.example.attendanceapplication.Fragments.HomeFragment;
+import com.example.attendanceapplication.Fragments.ProfileFragment;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class SecondActivity extends AppCompatActivity {
 
-    private String adminEmail;
-    private FirebaseAuth mAuth;
-    private FirebaseUser user;
-
-    private Button btnCreateUser;
-    private Button btnLogout;
-    private TextView tvMsg;
-    private EditText etEmail;
-    private EditText etPassword;
+    FrameLayout frameLayout;
+    BottomNavigationView bottomNavigationView;
+    MaterialToolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        adminEmail = "admin@gm.com";
-        mAuth = FirebaseAuth.getInstance();
-        user = mAuth.getCurrentUser();
+        frameLayout = findViewById(R.id.fragment);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        toolbar = findViewById(R.id.toolbar);
 
-        btnCreateUser = findViewById(R.id.btnCreateUser);
-        btnLogout = findViewById(R.id.btnLogout);
-        tvMsg = findViewById(R.id.tvMsg);
-        etEmail = findViewById(R.id.etEmail1);
-        etPassword = findViewById(R.id.etPassword1);
-
-        if (user != null) {
-            String mail = user.getEmail();
-            tvMsg.setText(String.format("Hello %s", mail));
-
-            // Nếu không phải tài khoản của admin thì sẽ không hiện phần đăng kí user
-            if (!mail.equals(adminEmail)) {
-                etEmail.setVisibility(View.GONE);
-                etPassword.setVisibility(View.GONE);
-                btnCreateUser.setVisibility(View.GONE);
-            }
-        }
-
-        // Đăng xuất
-        btnLogout.setOnClickListener(new View.OnClickListener() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment, new AttendanceFragment()).commit();
+        toolbar.setTitle(getString(R.string.title_home));
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                mAuth.signOut();
-                startActivity(new Intent(SecondActivity.this, MainActivity.class));
-                finish();
-                Toast.makeText(SecondActivity.this, "Logout Successful.", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btnCreateUser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email = etEmail.getText().toString().trim();
-                String password = etPassword.getText().toString().trim();
-
-                // Kiểm tra email và pw đã được nhập hay chưa
-                if (email.equals("") || password.equals("")) {
-                    Toast.makeText(SecondActivity.this, "Email and password cannot be empty.", Toast.LENGTH_SHORT).show();
-                } else {
-                    // Đăng ký user
-                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                etEmail.getText().clear();
-                                etPassword.getText().clear();
-                                Toast.makeText(SecondActivity.this, "Create user successful.", Toast.LENGTH_SHORT).show();
-                            } else {
-                                Toast.makeText(SecondActivity.this, "Create user Failed.", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.home_menu) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment, new HomeFragment()).commit();
+                    toolbar.setTitle(getString(R.string.title_home));
+                    return true;
+                } else if (item.getItemId() == R.id.attendance_menu) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment, new AttendanceFragment()).commit();
+                    toolbar.setTitle(getString(R.string.title_attendance));
+                    return true;
+                } else if (item.getItemId() == R.id.profile_menu) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment, new ProfileFragment()).commit();
+                    toolbar.setTitle(getString(R.string.title_profile));
+                    return true;
                 }
+                return false;
             }
         });
     }
