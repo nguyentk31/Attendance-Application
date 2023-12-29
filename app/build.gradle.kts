ebb2fbbfed93040c0fb7d95d12ece57851cf889f
tuyendepzai <letuyenkhtn212@gmail.com>
2023-12-29 12:10:55 +0700

    calendar highlight

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment:2.5.3")
     implementation("androidx.navigation:navigation-ui:2.5.3")
     implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation ("com.prolificinteractive:material-calendarview:1.4.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
